1f2e85b1491f1f66d183f464657b4b1ef223ec99
Weerapat Techaudomtaworn <gotcha5009@hotmail.com>
2020-09-08 13:54:39 +0700

    API01 POST upload file and get metadata

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+
+const upload = multer();
 
 // require and use "multer"...
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get('/hello', function (req, res) {
+  res.json({ greetings: "Hello, API" });
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
 });
 
 app.listen(process.env.PORT || 3000, function () {
